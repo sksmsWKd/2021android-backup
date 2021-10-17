86ac177ad1c169e558ced2151b9d3406237ef100
Song Jae Hyeon <0nocsace@gmail.com>
2021-10-17 17:05:05 +0900

    10/17 android tabhost homework

--- TabHost/app/src/main/java/com/cookandroid/tabhost/MainActivity.java
+++ TabHost/app/src/main/java/com/cookandroid/tabhost/MainActivity.java
@@ -17,17 +17,21 @@ public class MainActivity extends TabActivity {
 
         TabHost tabHost = getTabHost();
 
-        TabHost.TabSpec tabSpecSong = tabHost.newTabSpec("SONG").setIndicator("음악별");
-        tabSpecSong.setContent(R.id.tabSong);
-        tabHost.addTab(tabSpecSong);
+        TabHost.TabSpec tabSpecDog = tabHost.newTabSpec("dog").setIndicator("강아지");
+        tabSpecDog.setContent(R.id.tabDog);
+        tabHost.addTab(tabSpecDog);
 
 
-        TabHost.TabSpec tabSpecArtist = tabHost.newTabSpec("ARTIST").setIndicator("가수별");
-        tabSpecSong.setContent(R.id.tabArtist);
-        tabHost.addTab(tabSpecArtist);
+        TabHost.TabSpec tabSpecCat = tabHost.newTabSpec("cat").setIndicator("고양이");
+        tabSpecCat.setContent(R.id.tabCat);
+        tabHost.addTab(tabSpecCat);
 
-        TabHost.TabSpec tabSpecAlbum = tabHost.newTabSpec("ALBUM").setIndicator("앨범별");
-        tabSpecSong.setContent(R.id.tabAlbum);
-        tabHost.addTab(tabSpecAlbum);
+        TabHost.TabSpec tabSpecRabbit = tabHost.newTabSpec("rabbit").setIndicator("토끼");
+        tabSpecRabbit.setContent(R.id.tabRabbit);
+        tabHost.addTab(tabSpecRabbit);
+
+        TabHost.TabSpec tabSpecHorse = tabHost.newTabSpec("horst").setIndicator("말");
+        tabSpecHorse.setContent(R.id.tabHorse);
+        tabHost.addTab(tabSpecHorse);
     }
 }
