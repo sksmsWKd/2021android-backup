d71e1bb7a2cc8ea0b6d13bb3185f3b239c4785d8
Song Jae Hyeon <0nocsace@gmail.com>
2021-11-11 23:16:56 +0900

    backup2

---
+++ app/src/main/java/com/cookandroid/a1109ch10/ThirdActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.a1109ch10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third);
+    }
+}
