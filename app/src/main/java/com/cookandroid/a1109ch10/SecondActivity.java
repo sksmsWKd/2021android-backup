ea54ae3940f71098e8a86d56af3a60fc3edbd4f5
Song Jae Hyeon <0nocsace@gmail.com>
2021-11-12 18:31:57 +0900

    rating masterpiece app finished

--- app/src/main/java/com/cookandroid/a1109ch10/SecondActivity.java
+++ app/src/main/java/com/cookandroid/a1109ch10/SecondActivity.java
@@ -2,13 +2,40 @@ package com.cookandroid.a1109ch10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView tvID,tvPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
+
+        tvID=findViewById(R.id.tvId);
+        tvPassword=findViewById(R.id.tvPassword);
+        Button btnReturn = (Button)findViewById(R.id.btnReturn);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+        Integer pw = intent.getIntExtra("password",0000);
+
+        tvID.setText(id.toString());
+        tvPassword.setText(pw.toString());
+
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/cookandroid/a1109ch10/ThirdActivity.java
+++ app/src/main/java/com/cookandroid/a1109ch10/ThirdActivity.java
@@ -3,12 +3,24 @@ package com.cookandroid.a1109ch10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third);
+
+        Button btnReturn = (Button)findViewById(R.id.btnReturn);
+
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
